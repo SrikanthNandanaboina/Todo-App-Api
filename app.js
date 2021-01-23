2e4f3306dfb146315b42877e1dbc475f4f25ea17
Srikanth N <srikanthnandhan@gmail.com>
2021-01-23 23:07:44 +0530

    updated port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = 9000;
+const port = process.env.PORT || 3000;
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
